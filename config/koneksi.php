dcaf6519ea6fbb4dc4d3468e519eaf490eb8f1c8
rahzaame <rahzaame@gmail.com>
2026-04-06 20:30:01 +0700

    koneksi

--- config/koneksi.php
+++ config/koneksi.php
@@ -5,9 +5,9 @@ $user = "root";
 $password = "";
 $database = "zahra_laundry";
 
-$conn = mysqli_connect($host,$user,$password,$database);
+$conn = mysqli_connect($host, $user, $password, $database);
 
-if(!$conn){
+if(!conn){
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 
