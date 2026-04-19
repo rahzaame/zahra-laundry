b8fba4cf786a2c386c76cef6c82effc2d53a0f90
rahzaame <rahzaame@gmail.com>
2026-04-19 16:49:11 +0700

    revisi logout

--- logout.php
+++ logout.php
@@ -1,20 +1,10 @@
 <?php
-
 session_start();
-
 $_SESSION = [];
-
 if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(
-        session_name(), '', time() - 42000,
-        $params["path"],    $params["domain"],
-        $params["secure"],  $params["httponly"]
-    );
+    $p = session_get_cookie_params();
+    setcookie(session_name(), '', time()-42000, $p["path"], $p["domain"], $p["secure"], $p["httponly"]);
 }
-
 session_destroy();
-
-header("Location: login.php?logout=1");
+header("Location: login.php");
 exit();
-?>
